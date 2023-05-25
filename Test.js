3354b07db70fbbaaf86c618be55fd9f49a96dcdf
Yun <92593553+YunDudali@users.noreply.github.com>
2023-05-25 16:38:20 -0400

    fishish - JDBC learning && front

---
+++ Test.js
@@ -0,0 +1,19 @@
+// const arr = [0, 2, 1, 5, 9, 3, 4, 6, 7, 8]
+//
+// for (let i = 0; i < arr.length; i++){
+//     //冒泡排序
+//     for (let j = 0; j < arr.length - i - 1; j++){
+//         if (arr[j+1] < arr[j]){
+//             let temp = arr[j + 1]
+//             arr[j+1] = arr[j]
+//             arr[j] = temp
+//         }
+//     }
+// }
+//
+// windew.alert(arr)
+// conole.info("")
+
+let a = 10
+a = "HelloWorld"
+console.info(a)
